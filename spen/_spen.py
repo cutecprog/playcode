10918082b39b8561965e9ad0ba0937e61570f2b5
Andrea Grooms <andi.grooms@gmail.com>
2012-05-08 12:03:39 -0500

    Implement basic TXT function with class version, starting html5 function

--- spen/_spen.py
+++ spen/_spen.py
@@ -1,8 +1,21 @@
 #!/bin/python
 
+'''
+%ANN'S MOM
+(Yelling from the other room
+"Come get dinner sweety!
+%ANN
+(Yelling back
+"I'm not hungry I ate on my way back from school like I always do. Save me some for later.
+%ANN'S MOM
+"What sweety? Woof!
+'''
+
 def main():
    test = spen()
-   test.add(action("Woof"))
+   test.add(slug(1, "ANN'S HOUSE", "DAY (NOON)"))
+   test.add(action("Ann lays in her room reading a book. And woofs around a whole bunch like crazy woofing."))
+   test.add(dialog("ANN'S MOM", "Yelling from the other room", "Come get dinner sweety!"))
    print test.formatTXT()
 
 # Generic line
@@ -10,7 +23,7 @@ class line(object):
    def __init__(self, content):
       self.content = content
       
-   def _set_margin(str,start,end):
+   def _set_margin(self,str,start,end):
       rowSize = end-start
       result = ""
 
@@ -32,15 +45,33 @@ class line(object):
 
 # Slug line   
 class slug(line):
-   pass
+   def __init__(self, light, location, time):
+      line.__init__(self,location)
+      self.light = light
+      self.time = time
+   def format(self):
+      str = "INT." if self.light else "EXT."
+      str += " "+self.content
+      str += " - "+self.time
+      return line._set_margin(self,str,17,74)
 
 # Action line      
 class action(line):
-   pass
+   def format(self):
+      return line._set_margin(self,self.content,17,74)
 
 # Dialog line      
 class dialog(line):
-   pass
+   def __init__(self, speaker, para, dialog):
+      line.__init__(self,dialog)
+      self.speaker = speaker
+      self.para = para
+   
+   def format(self):
+      str = line._set_margin(self,self.speaker,41,74)+"\n"
+      str += line._set_margin(self,"("+self.para,34,54)+")\n"
+      str += line._set_margin(self,self.content,21,61)
+      return str
 
 # Main class      
 class spen(object):
@@ -53,7 +84,7 @@ class spen(object):
    def formatTXT(self):
       sum_str = ""
       for l in self.lines:
-         sum_str += l.content
+         sum_str += l.format() + "\n\n"
       return sum_str
 
 
