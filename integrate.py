7cd81841fda0fdb221e0edcec1c87937df81e624
cutecprog <cutecprog@gmail.com>
2012-09-01 12:13:36 -0500

    Added comment for function that may have just made thing more confusing

--- integrate.py
+++ integrate.py
@@ -1,11 +1,17 @@
 from math import *
 
+"""
+This function estimates the integral of a function using right bound rectangles
+with a certain resolution n.
+
+sigma(low, high-1): f(i*width)*width
+where width = (high - low) / n
+"""
 def integrate(f, n, low, high):
         sig = 0
         width = (high - low) /n
         i = low
         while (i < high):
-                #print i/pi
                 sig += f(i)
                 i += width
         return sig*width
