b668bc850c087a8d4e491fbbba924711eb92286d
cutecprog <cutecprog@gmail.com>
2012-09-18 16:31:19 -0500

    Made some changes during calculus class. Now using simpson's law. Going to refactor to make it cleaner.

--- integrate.py
+++ integrate.py
@@ -1,8 +1,44 @@
 # Author: Andrea Sarah
 # Version: 12.09.02
 
+from math import *
+
 def main():
-        print "5.1: 8"
+        f = lambda x: 1/x
+        box = 2
+        a = integrate(f, box, 1, 2, "l")
+        b = integrate(f, box, 1, 2, "r")
+        c = integrate(f, box, 1, 2, "m")
+        d = a/6 + b/6 + (2*c/3)
+        print box
+        print d, log(2)-d
+
+        box = 10
+        a = integrate(f, box, 1, 2, "l")
+        b = integrate(f, box, 1, 2, "r")
+        c = integrate(f, box, 1, 2, "m")
+        d = a/6 + b/6 + (2*c/3)
+        print box
+        print d, log(2)-d
+
+        box = 50
+        a = integrate(f, box, 1, 2, "l")
+        b = integrate(f, box, 1, 2, "r")
+        c = integrate(f, box, 1, 2, "m")
+        d = a/6 + b/6 + (2*c/3)
+        print box
+        print d, log(2)-d
+
+        box = 250
+        a = integrate(f, box, 1, 2, "l")
+        b = integrate(f, box, 1, 2, "r")
+        c = integrate(f, box, 1, 2, "m")
+        d = a/6 + b/6 + (2*c/3)
+        print box
+        print d, log(2)-d
+        
+        
+        """print "5.1: 8"
         print integrate(cos, 10, 0, pi/2)
         print integrate(cos, 30, 0, pi/2)
         print integrate(cos, 50, 0, pi/2)
@@ -14,9 +50,14 @@ def main():
         print integrate(f, 30, 0, 2)
         print integrate(f, 50, 0, 2)
         print integrate(f, 100, 0, 2)
+        print "Left bound"
+        print integrate(f, 10, 0, 2, "l")
+        print integrate(f, 30, 0, 2, "l")
+        print integrate(f, 50, 0, 2, "l")
+        print integrate(f, 100, 0, 2, "l")"""
 
-def integrate(f, n, low, high, mode="r"):
-        """Compute mode bound Riemann's sum and return answer
+def integrate(f, n, low, high, mode="l"):
+        """Compute mode bound Riemann's sum and return answer.
 
         This function estimates the integral from low to high of a function
         using mode bound rectangles with a certain resolution n.
@@ -25,12 +66,12 @@ def integrate(f, n, low, high, mode="r"):
         total = 0
         width = float((high - low)) / n
         i = low
-        if mode == "l":
+        if mode == "r":
                 i += width
                 high += width
         elif mode == "m":
                 i += width / 2
-                high += width /2
+                high += width / 2
         while i < high:
                 total += f(i)
                 i += width
