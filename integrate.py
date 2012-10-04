24ffda40b498e42807b5f914d2219d8e6d34c8c2
cutecprog <cutecprog@gmail.com>
2012-10-04 11:41:38 -0500

    Added simpson_rule2() that using exact values instead of a function definition

--- integrate.py
+++ integrate.py
@@ -10,12 +10,27 @@ def main():
         print "Natural log of 2"
         print "About:", simpson_rule(f, n, low, high)
         print "Exact:", F(high) - F(low)
+        print simpson_rule2([5.8, 20.3, 26.7, 29.0, 27.6, 27.3, 23.8, 20.5, 15.1], 20)
+
+def simpson_rule2(f, h):
+        """Use a list of exact values to compute simpson's rule.
+
+        """
+        l = len(f)
+        S = f[0] + f[l-1]
+        for i in range(1, l - 1):
+                if i % 2:
+                        S += 4*f[i]
+                else:
+                        S += 2*f[i]
+        return float(S * h) / 3
 
 def simpson_rule(f, n, low, high):
-        """Use Riemann's sum to compute Simpson's rule and return answer.
+        """Use function definition to compute Simpson's rule.
 
         This function estimates the integral from low to high of a function
-        using Simpson's rule.
+        using Simpson's rule. Use Riemann's Sum to compute left, right, and mid
+        end point approximations.
 
         """
         l = riemann_sum(f, n, low, high, "l")
