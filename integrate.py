5c3d09457426043e86b49f049d3c7e3ae9684640
cutecprog <cutecprog@gmail.com>
2012-10-04 10:59:30 -0500

    Minor refactoring.

--- integrate.py
+++ integrate.py
@@ -1,37 +1,28 @@
 # Author: Andrea Sarah
-# Version: 12.09.02
+# Version: 12.10.04
 
 def main():
         f = lambda x: 1 / x
         F = log
+        n = 64
         low = 1
         high = 2
-        for i in range(1, 21):
-                print i
-                start = clock()
-                tmp = simpson_rule(f, 2**i, low, high) - (F(high) - F(low))
-                end = clock()
-                print "S(%i) = " % 2**i, tmp, end - start
-                start = clock()
-                tmp = simpson_rule(f, 2**i, low, high, 0) - (F(high) - F(low))
-                end = clock()
-                print "S(%i) = " % (2**i), tmp, end - start 
-        
+        print "Natural log of 2"
+        print "About:", simpson_rule(f, n, low, high)
+        print "Exact:", F(high) - F(low)
 
-def simpson_rule(f, n, low, high, mode=1):
+def simpson_rule(f, n, low, high):
         """Use Riemann's sum to compute Simpson's rule and return answer.
 
-        This function estimates the intefral from low to high of a function
+        This function estimates the integral from low to high of a function
         using Simpson's rule.
 
         """
         l = riemann_sum(f, n, low, high, "l")
         r = riemann_sum(f, n, low, high, "r")
         m = riemann_sum(f, n, low, high, "m")
-        if mode == 1:
-                return (((l + r) / 2) + (2 * m)) / 3
-        else:
-                return (l / 6) + (r / 6) + (2 * m / 3)
+        t = (l + r) / 2
+        return (t + (2 * m)) / 3
 
 def riemann_sum(f, n, low, high, mode="m"):
         """Compute mode bound Riemann's sum and return answer.
