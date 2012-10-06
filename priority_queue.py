3a8c315e731ed5dde87cc150e9d8f0dd4c1c20ff
cutecprog <cutecprog@gmail.com>
2012-10-06 18:34:29 -0500

    Debugging priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -3,23 +3,33 @@ class priority_queue(object):
                 self.data = values
                 self.size = len(values)
         def pop(self):
-                tmp = self.data[size-1]
-                self.data = self.data[:size-2]
-                size--
+                tmp = self.data[self.size-1]
+                self.data = self.data[:self.size-1]
+                self.size -= 1
                 return tmp
         def least(self):
-                return self.sata[0]
+                return self.data[0]
+        def most(self):
+                return self.data[self.size-1]
         def add(self, value):
-                for i in range(0, size):
+                if value > self.most():
+                        self.data += [value]
+                        self.size += 1
+                        return
+                for i in range(0, self.size-1):
                         if value <= self.data[i]:
-                                self.data=self.data[:i] + [value] + self.data[i:]
-                                size++
+                                self.data = self.data[:i] + [value] + self.data[i:]
+                                self.size += 1
                                 return
-                self.data += [value]
 
 def main():
         L = priority_queue([54,78,93,103,5003])
-        print L.data
+        while L.size > 1:
+                print L.data
+                tmp = L.pop()%L.least()
+                print tmp
+                if tmp != 0:
+                        L.add(tmp)
 
-if __name__=="__main()__":
+if __name__=="__main__":
         main()
