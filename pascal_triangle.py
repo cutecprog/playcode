f5151dd7c1901d952bd1456bcd12ec60d089e049
cutecprog <cutecprog@gmail.com>
2012-10-19 22:12:32 -0500

    pascal_triangle.py made it indexable :D.

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,10 +1,12 @@
 def main():
+        print pascal_triangle(4,3)
+
+def pascal_triangle(row, col):
         gray_theory = lambda last, row, col: last * ((row + 1) - col) / col
-        for i in range(0, 100):
-                p = 1
-                for j in range(1, (i+2)/2):
-                        p = gray_theory(p, i, j)
-                print p
+        p = 1
+        for i in range(1, col):
+                p = gray_theory(p, row, i)
+        return p
 
 if __name__=="__main__":
         main()
