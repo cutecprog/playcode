32448e0a03d6bda4aa7e11417fc7d3bce86c4756
cutecprog <cutecprog@gmail.com>
2013-01-10 10:58:20 -0700

    Experimenting: Selected relevant section and saved source manually. Then parsed it.

--- rate_crawler.py
+++ rate_crawler.py
@@ -5,20 +5,32 @@ import urllib2
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-	pass
-        #print "Encountered a start tag:", tag
-    def handle_endtag(self, tag):
-	pass
-        #print "Encountered an end tag :", tag
-    def handle_data(self, data):
-        if data != ' ':
-                print "Encountered some data  :", data
+        data = ''
+        def handle_starttag(self, tag, attrs):
+        	pass
+                #print "Encountered a start tag:", tag
+        def handle_endtag(self, tag):
+	        pass
+                #print "Encountered an end tag :", tag
+        def handle_data(self, data):
+                if data != ' ':
+                        self.data += data + '\n'
 
 # instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
 
-u = urllib2.urlopen('http://www.gunowners.org/113srat.htm')
-s = ' '.join(u.read().split())
+#u = urllib2.urlopen('http://www.gunowners.org/113srat.htm')
+#s = ' '.join(u.read().split())
 #print s
+#parser.feed(s)
+#print parser.data
+
+f = open("./tmp_data", "r")
+
+u = f.read()
+s = ' '.join(u.split())
 parser.feed(s)
+print parser.data
+
+f.close()
+
