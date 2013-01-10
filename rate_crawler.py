f5976ce4a733a84ea7328112af83a675a46214ab
cutecprog <cutecprog@gmail.com>
2013-01-10 11:58:58 -0700

    Experimenting: Played with selenium and commented it out. Sorted data

--- rate_crawler.py
+++ rate_crawler.py
@@ -1,11 +1,20 @@
-print "hello CS internship"
-
 from HTMLParser import HTMLParser
 import urllib2
+"""from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("selenium")
+elem.send_keys(Keys.RETURN)
+assert "Google" in driver.title
+#driver.close()"""
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
-        data = ''
+        data = []
         def handle_starttag(self, tag, attrs):
         	pass
                 #print "Encountered a start tag:", tag
@@ -14,7 +23,7 @@ class MyHTMLParser(HTMLParser):
                 #print "Encountered an end tag :", tag
         def handle_data(self, data):
                 if data != ' ':
-                        self.data += data + '\n'
+                        self.data.append(data)
 
 # instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
@@ -30,7 +39,29 @@ f = open("./tmp_data", "r")
 u = f.read()
 s = ' '.join(u.split())
 parser.feed(s)
-print parser.data
-
+#print parser.data
 f.close()
 
+i = 0
+j = 0
+data = []
+data2 = []
+for n in parser.data:
+        if i%10 == 0 or i%10 == 1:
+                data.append(n.strip() + ": ")
+        else:
+                data2.append(n)
+        i += 1
+
+i = 0
+j = 0
+for n in data:
+        for i in range(0,4):
+                data[j] += data2.pop(0)
+                if (i + 3) % 4 == 0:
+                        data[j] += ","
+                data[j] += " "
+        j += 1
+
+for n in data:
+        print n
