00274000835a894fe1fb68b09c32f61003ef4dc5
cutecprog <cutecprog@gmail.com>
2013-01-11 13:43:11 -0700

    uim.py: Accessing arguements.

---
+++ uim.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+        print "Unique ID Matcher"
+        print 'Number of arguments:', len(sys.argv), 'arguments.'
+        print 'Argument List:', sys.argv
+
+if __name__=="__main__":
+        main()
