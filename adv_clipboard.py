70041ad8d8db12daecaf45544c2704825157aae0
cutecprog <cutecprog@gmail.com>
2013-01-20 16:49:32 -0600

    Advanced clipboard will just use multiple files. Probably going to switch over to one binary file for all pvs_tools memory but for now this is fine. This works.

--- adv_clipboard.py
+++ adv_clipboard.py
@@ -8,21 +8,9 @@ pygtk.require('2.0')
 cb = gtk.clipboard_get()
 
 if argv[1] == 's':
-        with open("./clipboard", "w") as f:
+        with open("./c" + argv[2], "w") as f:
                 f.write(cb.wait_for_text())
 else:
-        with open("./clipboard", "r") as f:
+        with open("./c" + argv[1], "r") as f:
                 cb.set_text(f.read())
                 cb.store()
-
-def file_set(f, index):
-        """Sets clipboard file to current copied text at index.
-        
-        """
-        pass
-
-def file_get(f, index):
-        """Returns desire data from clipboard file.
-        
-        """
-        pass
