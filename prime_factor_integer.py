c6e1aa91291a1b06efad1ff986f35d68b20ae4a1
cutecprog <cutecprog@gmail.com>
2013-05-29 17:31:24 -0500

    Trashed my code to do a little experimentation. Most girl experiment at this age anyways; I'm not ashamed.

--- prime_factor_integer.py
+++ prime_factor_integer.py
@@ -1,12 +1,22 @@
+import numpy as np
+import scipy.misc.pilutil as smp
+
 def main():
         from random import randint
         print "Main"
         ps = load_sieve()
-        last = 0
-        for p in ps:
-                print '\n' * (p - last - 1)
-                print 'X' * p
-                last = p
+        print len(ps)
+
+        # Create a 1024x1024x3 array of 8 bit unsigned integers
+        data = np.zeros( (10000,3000,3), dtype=np.uint8 )
+        for i in range(0, 10000):
+                #print i
+                s = itopfi(i,ps)
+                #print s
+                data[i] = bit_line(s)
+
+        img = smp.toimage( data )       # Create a PIL image
+        img.show()                      # View in default viewer
 
 def load_sieve(path = './prime_sieve'):
         """Load and return list (sieve) from a json data file.
@@ -34,6 +44,16 @@ def itopfi(n, prime_sieve = load_sieve()):
                         break
                 pfi += "0"
         return pfi
+
+def bit_line(pfi_string):
+        line = np.zeros( (3000,3), dtype=np.uint8 )
+        i = 0
+        for c in pfi_string:
+                if c == '1':
+                        line[i] = [255,255,255]
+                i += 1
+        return line
+                        
 
 if __name__ == "__main__":
         main()
