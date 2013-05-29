4fd2ff0c6bb4dad3fe706b69779f8911c354dc1c
cutecprog <cutecprog@gmail.com>
2013-05-29 13:15:03 -0500

    load_sieve() added an assert but it looks ugly.

--- prime_factor_integer.py
+++ prime_factor_integer.py
@@ -3,9 +3,16 @@ def main():
         print load_sieve()
 
 def load_sieve(path = './prime_sieve'):
+        """
+        Load and return list (sieve) from a json data file.
+
+        >>> import json
+        """
         import json
         with open(path, 'rb') as f:
                 sieve = json.loads(f.read())
+        assert type(sieve) == type(list()),\
+        "data file format incorrect" + " load_sieve(" + path + ") failed."
         return sieve
 
 if __name__ == "__main__":
