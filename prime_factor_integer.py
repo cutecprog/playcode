a88163f8a7f5c7d94816e3d8959dd6d84f747917
cutecprog <cutecprog@gmail.com>
2013-05-29 13:04:58 -0500

    load_sieve() one v1.

--- prime_factor_integer.py
+++ prime_factor_integer.py
@@ -1,5 +1,12 @@
 def main():
         print "test"
+        print load_sieve()
+
+def load_sieve(path = './prime_sieve'):
+        import json
+        with open(path, 'rb') as f:
+                sieve = json.loads(f.read())
+        return sieve
 
 if __name__ == "__main__":
         main()
