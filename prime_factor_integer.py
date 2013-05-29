1b29c1d9805b8436642e7abc80a025be5f7ea3d4
cutecprog <cutecprog@gmail.com>
2013-05-29 13:20:41 -0500

    load_sieve() added an assert but it looks ugly.

--- prime_factor_integer.py
+++ prime_factor_integer.py
@@ -1,12 +1,15 @@
 def main():
-        print "test"
-        print load_sieve()
+        print "Main"
+        ps = load_sieve()
+        itopfi(3)
+        tmp = input()
+        itopfi(3,ps)
 
 def load_sieve(path = './prime_sieve'):
-        """
-        Load and return list (sieve) from a json data file.
+        """Load and return list (sieve) from a json data file.
 
         >>> import json
+
         """
         import json
         with open(path, 'rb') as f:
@@ -14,6 +17,12 @@ def load_sieve(path = './prime_sieve'):
         assert type(sieve) == type(list()),\
         "data file format incorrect" + " load_sieve(" + path + ") failed."
         return sieve
+
+def itopfi(integer, prime_sieve = load_sieve()):
+        """Convert integer to string of prime factors.
+
+        """
+        print prime_sieve
 
 if __name__ == "__main__":
         main()
