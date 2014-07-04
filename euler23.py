24a68d876f9528da43b60e395a677c20f7c5748d
cutecprog <andi.grooms@gmail.com>
2014-07-03 19:08:51 -0500

    Boiler plate for euler23.py

---
+++ euler23.py
@@ -0,0 +1,16 @@
+def main():
+        print "euler23.py"
+
+def proper_factors(n):
+        """Calculate proper factors of n.
+
+        >>> proper_factors(28)
+        [1, 2, 4, 7, 14]
+
+        """
+        pf = []
+        #for i in range(2, n/2):
+        return pf
+
+if __name__ == "__main__":
+        main()
