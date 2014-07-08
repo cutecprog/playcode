f730002bb19338a7db4a37d34241b760bc4da942
cutecprog <andi.grooms@gmail.com>
2014-07-08 12:01:52 -0500

    ascuii_map boilerplate.

--- euler23.py
+++ euler23.py
@@ -3,6 +3,7 @@ def main():
         print sum(proper_factors(28))
         print deficient(28)
         print perfect(28)
+        print ascuii_map([(lambda(n): n%2 == 0), nil], [' ', 'X'], ['list', 0, 12, 3])
 
 def proper_factors(n):
         """Calculate proper factors of n.
@@ -18,11 +19,25 @@ def proper_factors(n):
         
         return pf
 
-def deficient(n):
-        return sum(proper_factors(n)) < n
+deficient = lambda(n): sum(proper_factors(n)) < n
+perfect = lambda(n): sum(proper_factors(n)) == n
+nil = lambda(n): True # for else condition
 
-def perfect(n):
-        return sum(proper_factors(n)) == n
+def ascuii_map(condition, symbol, shape):
+        """Make map of range numbers using ACUII symbols.
+        
+        Determine symbol in space with associated condition. Determine what
+        number a space represents with shape.
+
+        >>> ascuii_map([(lambda(n): n%2 == 0), nil], [' ', 'X'], ['list', 0, 12, 3])
+         X 
+        X X
+         X
+        X X
+        """
+        return "Map time!"
 
 if __name__ == "__main__":
+        from doctest import testmod
+        testmod()
         main()
